1aead3de3e9b4906852f1ea099fa66c19f7c263b
David Shanske <david@shanske.com>
2020-01-01 17:23:58 +0000

    Fix issue caused by removal of webmention class

--- semantic-linkbacks.php
+++ semantic-linkbacks.php
@@ -61,7 +61,7 @@ class Semantic_Linkbacks_Plugin {
 
 	public static function admin_init() {
 		self::privacy_declaration();
-		$page = class_exists( 'Webmention_Plugin' ) ? 'webmention' : 'discussion';
+		$page = function_exists( 'webmention_init' ) ? 'webmention' : 'discussion';
 		add_settings_section(
 			'semantic-linkbacks',
 			__( 'Semantic Linkbacks Settings', 'semantic-linkbacks' ),
@@ -99,7 +99,7 @@ class Semantic_Linkbacks_Plugin {
 	}
 
 	public static function register_settings() {
-		$option_group = class_exists( 'Webmention_Plugin' ) ? 'webmention' : 'discussion';
+		$option_group = function_exists( 'webmention_init' ) ? 'webmention' : 'discussion';
 		register_setting(
 			$option_group,
 			'semantic_linkbacks_facepiles',
