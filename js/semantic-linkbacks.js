d74cc990cdcdef9d7beeb0f84a2536c801c09f78
Florian Brinkmann <kontakt@florianbrinkmann.com>
2020-10-20 17:59:33 +0200

    Rework of feature to toggle additional facepiles

--- js/semantic-linkbacks.js
+++ js/semantic-linkbacks.js
@@ -1,7 +1,43 @@
-jQuery( document ).ready( function() {
-  jQuery( '.additional-facepile' ).hide();
+document.addEventListener( 'DOMContentLoaded', function() {
+  var showAdditionalFacepileButtons = document.querySelectorAll( '.show-additional-facepiles' ),
+    mentionLists = document.querySelectorAll( '.mention-list' );
 
-  jQuery( '.toggle-additional-facepiles' ).click( function() {
-    jQuery( this ).parent( 'ul' ).find( '.additional-facepile' ).toggle();
-  } );
-} );
+  if ( showAdditionalFacepileButtons.length === 0 || mentionLists.length === 0 ) {
+    return;
+  }
+
+  // Add `initialized` class to mention-list container. When JS is disabled or not working, we want to show all items.
+  for ( var i = 0; i < mentionLists.length; i++ ) {
+    var mentionList = mentionLists[i];
+    mentionList.classList.add( 'initialized' );
+  }
+
+  // Loop the buttons to show additional facepiles.
+  for ( var i = 0; i < showAdditionalFacepileButtons.length; i++ ) {
+    var showAdditionalFacepileButton = showAdditionalFacepileButtons[i],
+      hideAdditionalFacepileButton = showAdditionalFacepileButton.parentNode.parentNode.querySelector( '.hide-additional-facepiles' );
+
+    showAdditionalFacepileButton.addEventListener( 'click', function() {
+      toggleListItemClasses( this );
+    } );
+
+    hideAdditionalFacepileButton.addEventListener( 'click', function() {
+      toggleListItemClasses( this );
+    } );
+  }
+
+  /**
+   * Toggle the classes of the list items that contain the buttons.
+   *
+   * @param {HTMLElement} clickedButton
+   */
+  var toggleListItemClasses = function( clickedButton ) {
+    var buttonListItem = clickedButton.parentNode,
+    otherButtonListItem = buttonListItem.classList.contains( 'is-hidden' )
+      ? buttonListItem.parentNode.querySelector( '.additional-facepile-button-list-item:not(.is-hidden)' )
+      : buttonListItem.parentNode.querySelector( '.additional-facepile-button-list-item.is-hidden' );
+
+    buttonListItem.classList.toggle( 'is-hidden' );
+    otherButtonListItem.classList.toggle( 'is-hidden' );
+  }
+});
